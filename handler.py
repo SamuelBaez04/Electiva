2840a55d5c29b7c2307241030db642d41c633d96
Samuel Baez <sb269522@gmail.com>
2025-02-26 09:22:22 -0500

    Initial Commit

---
+++ handler.py
@@ -0,0 +1,15 @@
+import json
+
+
+def informacion(event, context):
+    body = {
+        "nombre": "Samuel",
+        "apellidos": "Valencia Baez",
+        "edad": 18,
+        "programa" : "Ingenieria de Software",
+        "codigo" : "240220232015"
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
